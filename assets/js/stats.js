a24ca2800447cfff2210886781d0abc3b65d6ff1
vjrj <vjrj@ourproject.org>
2020-01-13 21:11:07 +0100

    Fix typo in stats

--- assets/js/stats.js
+++ assets/js/stats.js
@@ -1,32 +1,34 @@
 function loadStats(){
 
-   $.getJSON("https://registros-ws.gbif.es/occurrences/search?pageSize=0", function( data ) {
-      $('#number_registros').html(addPeriods(data.totalRecords)).addClass('loaded_stats');
-   });
+  $.getJSON("https://registros-ws.gbif.es/occurrences/search?pageSize=0", function( data ) {
+    $('#number_registros').html(addPeriods(data.totalRecords)).addClass('loaded_stats');
+  });
 
-   $.getJSON("https://colecciones.gbif.es/ws/institution", function( data ) {
-      $('#number_instituciones').html(data.length).addClass('loaded_stats');
-   });
+  $.getJSON("https://colecciones.gbif.es/ws/institution", function( data ) {
+    $('#number_instituciones').html(data.length).addClass('loaded_stats');
+  });
 
-   $.getJSON("https://colecciones.gbif.es/ws/dataResource", function( data ) {
-      $('#number_bases').html(data.length).addClass('loaded_stats');
-   }); 
+  $.getJSON("https://colecciones.gbif.es/ws/dataResource", function( data ) {
+    $('#number_bases').html(data.length).addClass('loaded_stats');
+  });
 
-   $.getJSON("https://registros-ws.gbif.es/occurrence/facets?q=*:*&facets=species&pageSize=0", function(data) {
-      $("#number_species").html(addPeriods(data[0].count)).addClass('loaded_stats');
-   });
+  $.getJSON("https://registros-ws.gbif.es/occurrence/facets?q=*:*&facets=species&pageSize=0", function(data) {
+    $("#number_species").html(addPeriods(data[0].count)).addClass('loaded_stats');
+  });
 }
 
 
 function addPeriods(nStr)
 {
-    nStr += '';
-    x = nStr.split('.');
-    x1 = x[0];
-    x2 = x.length > 1 ? '.' + x[1] : '';
-    var rgx = /(\d+)(\d{3})/;
-    while (rgx.test(x1)) {
-        x1 = x1.replace(rgx, '$1' + '.' + '$2');
-    }
-    return x1 + x2;
+  nStr += '';
+  var sep = locale === 'en' ? ',': '.';
+  var x = nStr.split('.');
+  var x1 = x[0];
+  var x2 = x.length > 1 ? '.' + x[1] : '';
+
+  var rgx = /(\d+)(\d{3})/;
+  while (rgx.test(x1)) {
+    x1 = x1.replace(rgx, '$1' + sep + '$2');
+  }
+  return x1 + x2;
 }
