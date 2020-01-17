b8d4a4228f758262016cf83dbdbd520f10dc702c
vjrj <vjrj@ourproject.org>
2020-01-17 02:00:49 +0100

    Added eslint

--- gulpfile.js
+++ gulpfile.js
@@ -25,6 +25,8 @@ const browserSync = require('browser-sync').create();
 
 const log = require('fancy-log');
 
+const eslint = require('gulp-eslint');
+
 const { src, dest } = gulp;
 
 const paths = {
@@ -232,6 +234,15 @@ function autocompleteJS() {
 function otherJsFiles() {
   return src(paths.js.src)
     .pipe(dest(paths.js.dest))
+  // eslint() attaches the lint output to the "eslint" property
+  // of the file object so it can be used by other modules.
+    .pipe(eslint())
+  // eslint.format() outputs the lint results to the console.
+  // Alternatively use eslint.formatEach() (see Docs).
+    .pipe(eslint.format())
+  // To have the process exit with an error code (1) on
+  // lint error, return the stream and pipe to failAfterError last.
+    .pipe(eslint.failAfterError())
     .pipe(babel({
       presets: ['@babel/preset-env']
     }))
